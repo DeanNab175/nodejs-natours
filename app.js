f7292f568df5813015589fa0992609477a14f52a
deannab175 <f.jscript@gmail.com>
2024-09-08 14:24:01 +0400

    add delete api

--- app.js
+++ app.js
@@ -61,7 +61,10 @@ app.post('/api/v1/tours', (req, res) => {
 
 app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
   }
 
   res.status(200).json({
@@ -69,6 +72,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     data: {
       tour: '<Updated tour here...>',
     },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
